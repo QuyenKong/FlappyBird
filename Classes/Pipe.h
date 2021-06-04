0b39b6088cc09270fdbb3857096fb73438504ebb
QuyenKong <qdcong2209@gmail.com>
2021-06-04 23:29:53 +0700

    FlappyBird(04/06/2021)

---
+++ Classes/Pipe.h
@@ -0,0 +1,17 @@
+#ifndef __PiPe_H__
+#define __PiPe_H__
+
+#include "cocos2d.h"
+
+class Pipe 
+{
+public:
+	Pipe();
+	void SpawnPipe(cocos2d::Layer *layer);
+	
+private:
+	cocos2d::Size visibleSize;
+	cocos2d::Vec2 origin;
+};
+
+#endif // __PiPe_H__
